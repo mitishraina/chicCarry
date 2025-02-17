17c0a2f79e9069d74099d8610b35615d9fd7cde4
Mitish Raina <rainamitish7@gmail.com>
2025-02-17 19:30:59 +0530

    done with separation of concerns in models and created routes

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
-
 const cookieParser = require ('cookie-parser');
 const path = require('path');
+const db = require("./config/mongoose-connection");
+const ownersRouter = require("./routes/ownersRouter")
+const usersRouter = require("./routes/usersRouter")
+const productsRouter = require("./routes/productsRouter")
 
 app.use(express.json());
 app.use(express.urlencoded({ extended:true }));
@@ -10,9 +13,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 
-app.get('/', (req, res) => {
-    res.send("hey");
-});
+app.use("/owners", ownersRouter);
+app.use("/users", usersRouter);
+app.use("/products", productsRouter);
 
 app.listen(3000);
 console.log(`server is running on http://localhost:3000`);
--- models/user-model.js
+++ models/user-model.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://127.0.0.1:27017/chicCarry");
-
 const userSchema = mongoose.Schema({
     fullname: String,
+    email: String,
     password: String,
     cart: {
         type: Array,
