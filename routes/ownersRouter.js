27eb3c3c082e9aa45ac534efce8a02a66135074a
Mitish Raina <rainamitish7@gmail.com>
2025-02-17 21:14:40 +0530

    handled routes and created owner

--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,8 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const ownerModel = require('../models/owner-model');
+
+if(process.env.NODE_ENV === "development"){
+    router.post("/create", async function(req,res) {
+        let owners = await ownerModel.find();
+        if(owners.length > 0) 
+            {return res
+                .status(503)
+                .send("YOU DON'T HAVE PERMISSION TO CREATE A NEW OWNER")
+            }
+            let {fullname, email, password} = req.body;
+            let createdOwner = await ownerModel.create({
+                fullname,
+                email,
+                password,
+            });
+            res.status(201).send(createdOwner);
+    })
+}
 
 router.get("/", function(req, res) {
     res.send("hey its working owners:)");
 })
+
 
 module.exports = router;
