39b2927f6140ca97805d776f763aba20967ca8bd
Mitish Raina <rainamitish7@gmail.com>
2025-02-18 01:06:36 +0530

    basic auth done, now moving onto setting up middlewares

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const { registerUser, loginUser } = require("../controllers/authController");
 
 router.get("/", function(req, res) {
     res.send("hey its working users:)");
 })
+
+router.post("/register", registerUser);
+router.post("/login", loginUser)
 
 module.exports = router;
